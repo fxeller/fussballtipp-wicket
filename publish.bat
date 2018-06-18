c27ebb0b20884b5abe029ec1c7c370fea612c45e
Florian Xeller <fxeller@delmak.de>
2018-06-18 09:42:06 +0200

    Changes in deployment

--- publish.bat
+++ publish.bat
@@ -1,5 +1,6 @@
-pscp ./target/fussballtipp-1.0.war fxeller@emavok.com:/home/fxeller/fussballtipp.war
+pscp ./target/fussballtipp-1.0.war fxeller@emavok.com:/home/fxeller/fussballtipp-1.0.war
+rem pscp ./target/fussballtipp-1.0.war fxeller@emavok.com:/home/fxeller/fussballtipp.war
 
-rem sudo mv /home/fxeller/fussballtipp.war /opt/tomcat/webapps
-rem sudo chown tomcat:tomcat /opt/tomcat/webapps/fussballtipp.war
+rem sudo cp /home/fxeller/fussballtipp-1.0.war /opt/tomcat/webapps/ROOT.war
+rem sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 rem sudo systemctl restart tomcat.service
